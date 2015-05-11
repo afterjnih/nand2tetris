b13600e894c0bcdc70ebfe74cff6cdc274cfd2f9
Koji Ota <afterjnih@gmail.com>
2015-05-12 00:20:25 +0900

    solve project07 stage#2

--- 07/CodeWriter.rb
+++ 07/CodeWriter.rb
@@ -4,8 +4,8 @@ class CodeWriter
     @i = 0
   end
 
-  def setFileName(fileName)
-
+  def setFileName(file_name)
+    @asm_file_name = file_name
   end
 
   def writeArithmetic(command)
@@ -109,13 +109,249 @@ class CodeWriter
   end
 
   def writePushPop(command, segment, index)
-    @asm_file.puts '@' + index.to_s
-    @asm_file.puts 'D=A'
-    @asm_file.puts '@SP'
-    @asm_file.puts 'A=M'
-    @asm_file.puts 'M=D'
-    @asm_file.puts '@SP'
-    @asm_file.puts 'M=M+1'
+    if command ==  'C_PUSH'
+      case segment
+      when 'constant'
+        @asm_file.puts '@' + index.to_s
+        @asm_file.puts 'D=A'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'A=M'
+        @asm_file.puts 'M=D'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'M=M+1'
+      when 'local'
+        @asm_file.puts '@LCL'
+        @asm_file.puts 'D=M'
+        @asm_file.puts '@' + index.to_s
+        @asm_file.puts 'A=D+A'
+        @asm_file.puts 'D=M'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'A=M'
+        @asm_file.puts 'M=D'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'M=M+1'
+      when 'argument'
+        @asm_file.puts '@ARG'
+        @asm_file.puts 'D=M'
+        @asm_file.puts '@' + index.to_s
+        @asm_file.puts 'A=D+A'
+        @asm_file.puts 'D=M'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'A=M'
+        @asm_file.puts 'M=D'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'M=M+1'
+      when 'this'
+        @asm_file.puts '@THIS'
+        @asm_file.puts 'D=M'
+        @asm_file.puts '@' + index.to_s
+        @asm_file.puts 'A=D+A'
+        @asm_file.puts 'D=M'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'A=M'
+        @asm_file.puts 'M=D'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'M=M+1'
+      when 'that'
+        @asm_file.puts '@THAT'
+        @asm_file.puts 'D=M'
+        @asm_file.puts '@' + index.to_s
+        @asm_file.puts 'A=D+A'
+        @asm_file.puts 'D=M'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'A=M'
+        @asm_file.puts 'M=D'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'M=M+1'
+      when 'pointer'
+        @asm_file.puts '@R' + (3 + index).to_s
+        @asm_file.puts 'D=M'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'A=M'
+        @asm_file.puts 'M=D'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'M=M+1'
+      when 'temp'
+        @asm_file.puts '@R' + (5 + index).to_s
+        @asm_file.puts 'D=M'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'A=M'
+        @asm_file.puts 'M=D'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'M=M+1'
+      when 'static'
+        @asm_file.puts '@' + @asm_file_name + '.' + index.to_s
+        # @asm_file.puts '@' + (16 + index).to_s
+        @asm_file.puts 'D=M'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'A=M'
+        @asm_file.puts 'M=D'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'M=M+1'
+      end
+    else
+      case segment
+      when 'constant'
+      when 'local'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'A=M-1'
+        @asm_file.puts 'D=M'
+
+        @asm_file.puts '@R13'
+        @asm_file.puts 'M=D'
+
+        @asm_file.puts '@LCL'
+        @asm_file.puts 'D=M'
+        @asm_file.puts '@' + index.to_s
+        @asm_file.puts 'D=D+A'
+
+        @asm_file.puts '@R14'
+        @asm_file.puts 'M=D'
+
+        @asm_file.puts '@R13'
+        @asm_file.puts 'D=M'
+
+        @asm_file.puts '@R14'
+        @asm_file.puts 'A=M'
+        @asm_file.puts 'M=D'
+
+        @asm_file.puts '@SP'
+        @asm_file.puts 'M=M-1'
+
+        @asm_file.puts '@R13'
+        @asm_file.puts 'M=0'
+        @asm_file.puts '@R14'
+        @asm_file.puts 'M=0'
+      when 'argument'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'A=M-1'
+        @asm_file.puts 'D=M'
+
+        @asm_file.puts '@R13'
+        @asm_file.puts 'M=D'
+
+        @asm_file.puts '@ARG'
+        @asm_file.puts 'D=M'
+        @asm_file.puts '@' + index.to_s
+        @asm_file.puts 'D=D+A'
+
+        @asm_file.puts '@R14'
+        @asm_file.puts 'M=D'
+
+        @asm_file.puts '@R13'
+        @asm_file.puts 'D=M'
+
+        @asm_file.puts '@R14'
+        @asm_file.puts 'A=M'
+        @asm_file.puts 'M=D'
+
+        @asm_file.puts '@SP'
+        @asm_file.puts 'M=M-1'
+
+        @asm_file.puts '@R13'
+        @asm_file.puts 'M=0'
+        @asm_file.puts '@R14'
+        @asm_file.puts 'M=0'
+      when 'this'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'A=M-1'
+        @asm_file.puts 'D=M'
+
+        @asm_file.puts '@R13'
+        @asm_file.puts 'M=D'
+
+        @asm_file.puts '@THIS'
+        @asm_file.puts 'D=M'
+        @asm_file.puts '@' + index.to_s
+        @asm_file.puts 'D=D+A'
+
+        @asm_file.puts '@R14'
+        @asm_file.puts 'M=D'
+
+        @asm_file.puts '@R13'
+        @asm_file.puts 'D=M'
+
+        @asm_file.puts '@R14'
+        @asm_file.puts 'A=M'
+        @asm_file.puts 'M=D'
+
+        @asm_file.puts '@SP'
+        @asm_file.puts 'M=M-1'
+
+        @asm_file.puts '@R13'
+        @asm_file.puts 'M=0'
+        @asm_file.puts '@R14'
+        @asm_file.puts 'M=0'
+      when 'that'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'A=M-1'
+        @asm_file.puts 'D=M'
+
+        @asm_file.puts '@R13'
+        @asm_file.puts 'M=D'
+
+        @asm_file.puts '@THAT'
+        @asm_file.puts 'D=M'
+        @asm_file.puts '@' + index.to_s
+        @asm_file.puts 'D=D+A'
+
+        @asm_file.puts '@R14'
+        @asm_file.puts 'M=D'
+
+        @asm_file.puts '@R13'
+        @asm_file.puts 'D=M'
+
+        @asm_file.puts '@R14'
+        @asm_file.puts 'A=M'
+        @asm_file.puts 'M=D'
+
+        @asm_file.puts '@SP'
+        @asm_file.puts 'M=M-1'
+
+        @asm_file.puts '@R13'
+        @asm_file.puts 'M=0'
+        @asm_file.puts '@R14'
+        @asm_file.puts 'M=0'
+      when 'pointer'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'A=M-1'
+        @asm_file.puts 'D=M'
+        if index == 0
+          @asm_file.puts '@THIS'
+          @asm_file.puts 'M=D'
+        else
+          @asm_file.puts '@THAT'
+          @asm_file.puts 'M=D'
+        end
+        @asm_file.puts '@SP'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'A=M-1'
+        @asm_file.puts 'D=M'
+
+        @asm_file.puts '@R' + (5 + index).to_s
+        @asm_file.puts 'M=D'
+
+        @asm_file.puts '@SP'
+        @asm_file.puts 'M=M-1'
+      when 'temp'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'A=M-1'
+        @asm_file.puts 'D=M'
+        @asm_file.puts '@R' + (5 + index).to_s
+        @asm_file.puts 'M=D'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'M=M-1'
+      when 'static'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'A=M-1'
+        @asm_file.puts 'D=M'
+        @asm_file.puts '@' + @asm_file_name + '.' + index.to_s
+        # @asm_file.puts '@' + (16 + index).to_s
+        @asm_file.puts 'M=D'
+        @asm_file.puts '@SP'
+        @asm_file.puts 'M=M-1'
+      end
+    end
   end
 
   def end_line
--- 07/VMConverter.rb
+++ 07/VMConverter.rb
@@ -8,9 +8,8 @@ def convert(vm_file, code_writer)
     case parser.commandType
     when 'C_ARITHMETIC'
       code_writer.writeArithmetic(parser.arg1)
-    when 'C_PUSH'
+    when 'C_PUSH', 'C_POP'
       code_writer.writePushPop(parser.commandType, parser.arg1, parser.arg2)
-    when 'C_POP'
     when 'C_LABEL'
     when 'C_GOTO'
     when 'C_IF'
@@ -25,9 +24,11 @@ end
 if File.ftype(ARGV[0]) == 'directory'
   code_writer = CodeWriter.new(ARGV[0].split(nil)[0] + '.asm')
   Dir.open(ARGV[0]).each do |f|
+    code_writer.setFileName(f.split('.')[0])
     convert(ARGV[0] + '/' + f, code_writer) unless (f == '.') || (f == '..')
   end
 else
   code_writer = CodeWriter.new(ARGV[0].split('.')[0] + '.asm')
+  code_writer.setFileName(ARGV[0].split('.')[0])
   convert(ARGV[0], code_writer)
 end
